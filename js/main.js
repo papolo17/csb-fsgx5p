f4b5543ea6831181ecc1446313087aa3d65f789d
Code Sandbox <hello@codesandbox.io>
2022-09-02 01:25:41 +0000

    Deployment from Codesandbox.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("logo").addEventListener("click", (e) => {
+  document.classList.toggle("header");
+});
+
+document.getElementById("inicio").addEventListener("click", (e) => {
+  console.log("click");
+});
